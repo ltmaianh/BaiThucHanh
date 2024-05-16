2b2cdb8525c61eaa97b08cbd3c6031efef8049ae
ltmaianh <maianh2k2tx@gmail.com>
2024-05-16 23:47:02 +0700

    class Sinh vien

--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e557aa11ba695eb79a269cafa5a99c13a70ac40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+defd1c8c3160065357077f38571417c552011c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
