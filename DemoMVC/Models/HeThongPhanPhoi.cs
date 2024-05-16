b7494de07952e431eae5874d8e0e172b264f8d33
ltmaianh <maianh2k2tx@gmail.com>
2024-05-17 00:20:55 +0700

    class HeThongPhanPhoi

---
+++ DemoMVC/Models/HeThongPhanPhoi.cs
@@ -0,0 +1,6 @@
+namespace DemoMVC.Models{
+    public class HeThongPhanPhoi{
+        public string MaHTPP { get; set; }
+        public string TenHTPP { get; set; }
+    }
+}
--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b78b3dac25de6f0415627b12e7849c0a506a0ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdba45872113fe544df27a48e570c18504a813f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
